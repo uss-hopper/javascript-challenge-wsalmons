ebd46a31d8c86c23011f45e7258dcbd58e793591
Wyatt Salmons <wyattsalmons@gmail.com>
2019-02-20 16:47:07 -0700

    added additional functionality

--- script.js
+++ script.js
@@ -10,3 +10,27 @@ function drop(event) {
 	var data = event.dataTransfer.getData("text");
 	event.target.appendChild(document.getElementById(data));
 }
+
+function red() {
+	window.onclick = document.getElementById("para1").style.color = 'red';
+}
+
+function green() {
+	window.onclick = document.getElementById("para2").style.color = 'green';
+}
+
+function blue() {
+	window.onclick = document.getElementById("para3").style.color = 'blue';
+}
+
+function fontChange(paraId) {
+	window.onclick = document.getElementById(paraId).style.fontFamily = 'Roboto';
+}
+
+function fontChangePara2() {
+	window.onclick = document.getElementById("para2").style.fontFamily = 'cursive';
+}
+
+
+
+
